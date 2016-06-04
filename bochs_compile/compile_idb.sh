32259f3dc77e300a07b58852c11a5ec4bdc52ba2
secXsQuared <secXsQuared1995@gmail.com>
2016-06-04 16:14:50 -0700

    Added boots compilation stuff. Although bochs keeps seg faulting (lol wut) Now makefile generates disasm for debugging

---
+++ bochs_compile/compile_idb.sh
@@ -0,0 +1,23 @@
+cd src
+./configure --enable-smp \
+              --enable-cpu-level=6 \
+              --enable-all-optimizations \
+              --enable-x86-64 \
+              --enable-pci \
+              --enable-vmx \
+              --enable-debugger \
+              --enable-disasm \
+              --enable-debugger-gui \
+              --enable-logging \
+              --enable-fpu \
+              --enable-3dnow \
+              --enable-sb16=dummy \
+              --enable-cdrom \
+              --enable-x86-debugger \
+              --enable-iodebug \
+              --disable-plugins \
+              --disable-docbook \
+              --with-x --with-x11 --with-term --with-sdl
+
+make
+
